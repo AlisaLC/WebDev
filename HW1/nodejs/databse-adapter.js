32a0228ef173cc84ad8072903adc860a082f1fc7
shayan <shayanpardis82@gmail.com>
2021-10-14 23:38:00 +0330

    add simple-nodejs backend

---
+++ HW1/nodejs/databse-adapter.js
@@ -0,0 +1,36 @@
+const { Client } = require('pg')
+const { MongoClient } = require("mongodb");
+
+
+module.exports.PostgresRequester = function(user, database, password, host='localhost', ssl=false) {
+    return async (func)=>{
+        const client = new Client({
+            user,
+            database,
+            port: 5432,
+            host,
+            password,
+            ssl
+        })
+        try {
+            await client.connect()
+            return await func(client)    
+        } finally {
+            await client.end()
+        }
+    }
+}
+
+module.exports.MongoRequester = function(host) {
+    return async (func)=>{
+        const port = 27017
+        const uri = `${host}:${port}/SampleDB?retryWrites=true&writeConcern=majority`;
+        const client = new MongoClient(uri);
+        try {
+            await client.connect();
+            return await func(client)
+        } finally {
+            await client.close();
+        }
+    }
+}
---
+++ HW1/nodejs/index.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+const { PostgresRequester } = require("./databse-adapter.js")
+const postgres = PostgresRequester(user='postgres', database='postgres', password='1234')
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+app.use(express.static('../frontend'))
+
+const port = 3000
+
+app.get('/sha256', async (req, res) => {
+    const text = req.query.text
+    const hash = crypto.createHash('sha256')
+                   .update(text)
+                   .digest('base64');
+    // todo. only save for the first time
+    await postgres(async client=>client.query(`
+        INSERT INTO text2hash(text, hash)
+        VALUES ('${text}', '${hash}');
+    `))
+    res.send({hash: hash})
+})
+
+app.post('/sha256', async (req, res) => {
+    const hash = req.body.hash
+    const result = await postgres(async client=>client.query(`
+        SELECT text FROM text2hash
+        WHERE hash = '${hash}';
+    `))
+    res.send(result.rows[0])
+})
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
