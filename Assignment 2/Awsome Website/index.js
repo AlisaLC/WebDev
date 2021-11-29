1cf155b0b8493fda221ba5ba213beafa3b164636
AlisaLC <ali_sal1381@yahoo.com>
2021-11-29 16:19:13 +0330

    header improvement + code cleanup

--- Assignment 2/Awsome Website/index.js
+++ Assignment 2/Awsome Website/index.js
@@ -1,8 +1,9 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $(".change-color-btn").on('click', function () {
-        rnd = ()=>Math.floor(Math.random() * 256)
+        let getRandom = () => Math.floor(Math.random() * 256)
+        let getRandomColor = () => `${getRandom()}, ${getRandom()}, ${getRandom()}`
         $(".topSection").css({
-            backgroundImage: `linear-gradient(to bottom, rgba(${rnd()}, ${rnd()}, ${rnd()}, 0.52), rgba(${rnd()}, ${rnd()}, ${rnd()}, 0.73)), url(Images/IMG_7572.png)`
+            backgroundImage: `linear-gradient(to bottom, rgba(${getRandomColor()}, 0.52), rgba(${getRandomColor()}, 0.73)), url(Images/IMG_7572.png)`
         })
     })
 });
